d3933a19b3bdc2fccb604f8de1d8ad0cb51c6c1e
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-03-26 08:44:15 -0400

    Update Add Two Numbers.py

--- Add Two Numbers.py
+++ Add Two Numbers.py
@@ -16,33 +16,32 @@ class ListNode(object):
         self.val = x
         self.next = None
 
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        temp1 = l1
-        int1 = ''
-        while temp1 != None:
-            int1 += str(temp1.val)
-            temp1 = temp1.next  
-        
-        temp2 = l2
-        int2 = ''
-        while temp2 != None:
-            int2 += str(temp2.val)
-            temp2 = temp2.next
-        
-        int1 = int1[: : -1]
-        int2 = int2[: : -1]
-        result = str(int(int1) + int(int2))
+def addTwoNumbers(self, l1, l2):
+    """
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
+    temp1 = l1
+    int1 = ''
+    while temp1 != None:
+        int1 += str(temp1.val)
+        temp1 = temp1.next  
 
-        node = ListNode(result[0])
-        for i in range(1, len(result)):
-            temp = node
-            node = ListNode(int(result[i]))
-            node.next = temp
-        return node
-        
+    temp2 = l2
+    int2 = ''
+    while temp2 != None:
+        int2 += str(temp2.val)
+        temp2 = temp2.next
+
+    int1 = int1[: : -1]
+    int2 = int2[: : -1]
+    result = str(int(int1) + int(int2))
+
+    node = ListNode(result[0])
+    for i in range(1, len(result)):
+        temp = node
+        node = ListNode(int(result[i]))
+        node.next = temp
+    return node
+
