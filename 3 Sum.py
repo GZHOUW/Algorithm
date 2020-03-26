a70cb9d6896596bb5584e013a9c5120acda94f0e
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-03-26 08:42:17 -0400

    Update 3 Sum.py

--- 3 Sum.py
+++ 3 Sum.py
@@ -18,31 +18,29 @@ type nums: List[int]
 rtype: List[List[int]]
 
 '''
+def threeSum(self, nums):
 
-class Solution(object):
-    def threeSum(self, nums):
-
-        sol = []
-        length = len(nums)
-        nums.sort()
-        for a in range(length-2): # last two items cannot form a set of three
-            if a > 0 and nums[a] == nums[a-1]: # nums[a-1] is the previous target, already tested
-                continue
-            target = - nums[a] # - a = b + c
-            b = a + 1 # idx after target
-            c = length - 1 # last idx
-            while b < c:
-                if nums[b] + nums[c] == target:
-                    sol.append([nums[a], nums[b], nums[c]]) # sorted small - large
-                    while b < c and nums[b] == nums[b + 1]: # no need to test b+1 if they are same
-                        # b < c is for index error, ex: [0,0,0]
-                        b += 1
-                    while b < c and nums[c] == nums[c - 1]:
-                        c -= 1
+    sol = []
+    length = len(nums)
+    nums.sort()
+    for a in range(length-2): # last two items cannot form a set of three
+        if a > 0 and nums[a] == nums[a-1]: # nums[a-1] is the previous target, already tested
+            continue
+        target = - nums[a] # - a = b + c
+        b = a + 1 # idx after target
+        c = length - 1 # last idx
+        while b < c:
+            if nums[b] + nums[c] == target:
+                sol.append([nums[a], nums[b], nums[c]]) # sorted small - large
+                while b < c and nums[b] == nums[b + 1]: # no need to test b+1 if they are same
+                    # b < c is for index error, ex: [0,0,0]
                     b += 1
+                while b < c and nums[c] == nums[c - 1]:
                     c -= 1
-                elif nums[b] + nums[c] < target: # need to be larger
-                    b += 1
-                else:
-                    c -= 1
-        return sol
+                b += 1
+                c -= 1
+            elif nums[b] + nums[c] < target: # need to be larger
+                b += 1
+            else:
+                c -= 1
+    return sol
