2f077064522bcdff79ce58abc2586a60599fcbbf
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-03-26 12:18:20 -0400

    Update Longest Substring Without Repeating Characters.py

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -19,21 +19,20 @@ Explanation: The answer is "wke", with the length of 3.
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 '''
 
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        length = []
-        for i in range(len(s)):
-            substring = ''
-            idx = 0
-            while (i + idx < len(s)) and (s[i + idx] not in substring): 
-                # second condition is not evaluated if the first is False
-                substring += s[i + idx]
-                idx += 1
-            length.append(len(substring))
-        if length == []:
-            return 0
-        return max(length)
+def lengthOfLongestSubstring(self, s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    length = []
+    for i in range(len(s)):
+        substring = ''
+        idx = 0
+        while (i + idx < len(s)) and (s[i + idx] not in substring): 
+            # second condition is not evaluated if the first is False
+            substring += s[i + idx]
+            idx += 1
+        length.append(len(substring))
+    if length == []:
+        return 0
+    return max(length)
