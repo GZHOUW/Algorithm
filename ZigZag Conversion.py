7e59ce9528745f88bbdda87314e3558ba8d06c55
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-03-26 12:15:47 -0400

    Update ZigZag Conversion.py

--- ZigZag Conversion.py
+++ ZigZag Conversion.py
@@ -25,45 +25,43 @@ Y A   H R
 P     I
 '''
 
-class Solution(object):
-    def convert(self, s, numRows):
-        """
-        :type s: str
-        :type numRows: int
-        :rtype: str
-        """
-        if numRows == 1:
-            return s
-        zzList = []
-        for i in range(numRows):
-            zzList.append([])
-        length = len(s)
-        s_idx = 0
-        col_idx = 0
-        # add column by column
-        while s_idx < length:
-            if col_idx % (numRows - 1) == 0: # remainder = 0, fill all rows
-                for i in range(numRows):
-                    try:
-                        zzList[i].append(s[s_idx])
-                        s_idx += 1
-                    except IndexError: # ends at last column
-                        zzList[i].append('')
-                col_idx += 1
-            else: 
-                for i in range(numRows):
-                    if i == numRows - col_idx % (numRows - 1) - 1:
-                        zzList[i].append(s[s_idx])
-                        s_idx += 1
-                    else:
-                        zzList[i].append('')
-                col_idx += 1
-                
-        result = ''
-        for i in range(len(zzList)):
-            for j in range(len(zzList[0])):
-                if zzList[i][j] != '':
-                    result += zzList[i][j]
-        
-        return result
-                
+def convert(self, s, numRows):
+    """
+    :type s: str
+    :type numRows: int
+    :rtype: str
+    """
+    if numRows == 1:
+        return s
+    zzList = []
+    for i in range(numRows):
+        zzList.append([])
+    length = len(s)
+    s_idx = 0
+    col_idx = 0
+    # add column by column
+    while s_idx < length:
+        if col_idx % (numRows - 1) == 0: # remainder = 0, fill all rows
+            for i in range(numRows):
+                try:
+                    zzList[i].append(s[s_idx])
+                    s_idx += 1
+                except IndexError: # ends at last column
+                    zzList[i].append('')
+            col_idx += 1
+        else: 
+            for i in range(numRows):
+                if i == numRows - col_idx % (numRows - 1) - 1:
+                    zzList[i].append(s[s_idx])
+                    s_idx += 1
+                else:
+                    zzList[i].append('')
+            col_idx += 1
+
+    result = ''
+    for i in range(len(zzList)):
+        for j in range(len(zzList[0])):
+            if zzList[i][j] != '':
+                result += zzList[i][j]
+
+    return result
