63631c63d6b3c327ed93284b0dbe293c6face5e3
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-04-05 11:44:36 -0400

    Update Search in Rotated Sorted Array.py

--- Search in Rotated Sorted Array.py
+++ Search in Rotated Sorted Array.py
@@ -18,7 +18,7 @@ Example 2:
 Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
 '''
-def search(self, nums, target):
+def search(nums, target):
     length = len(nums)
     if length == 0:
         return -1
@@ -30,12 +30,12 @@ def search(self, nums, target):
         mid = (r - l)//2 + l # middle position between l and r
         if nums[mid] == target:
             return mid
-        elif nums[mid] >= nums[l]: # mid in left portion (if = , then mid is the leftmost)
-            if nums[mid] >= target and target >= nums[l]: # target between mid and l
-                r = mid -1
+        elif nums[mid] >= nums[l]: # mid is in left portion (if = , then mid is the leftmost)
+            if nums[mid] >= target and target >= nums[l]: # target is between mid and l
+                r = mid -1 # go left
             else:
-                l = mid + 1
-        else: # mid in right portion
+                l = mid + 1 # go right
+        else: # mid is in right portion
             if nums[mid] <= target and target <= nums[r]: # target between mid and r
                 l = mid + 1
             else:
