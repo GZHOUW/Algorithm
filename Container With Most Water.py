a61c8688b0ae4f1f41f0fb5cd60e23ba5f87a024
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-04-16 11:55:16 -0400

    Two Pointers

--- Container With Most Water.py
+++ Container With Most Water.py
@@ -1,5 +1,7 @@
 '''
-Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). 
+n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
+Find two lines, which together with x-axis forms a container, such that the container contains the most water.
 
 Note: You may not slant the container and n is at least 2.
 
@@ -9,21 +11,21 @@ Input: [1,8,6,2,5,4,8,3,7]
 Output: 49
 '''
 
-def maxArea(self, height):
+def maxArea(height):
     """
     :type height: List[int]
     :rtype: int
     """
     area_max = 0
-    l_idx = 0
-    r_idx = len(height) - 1
+    l = 0
+    r = len(height) - 1
 
-    while (l_idx < r_idx): # loop ends when r_idx - l_idx = 1
-        area_i = min(height[l_idx], height[r_idx]) * (r_idx - l_idx)
-        area_max = max(area_max, area_i)
-        if height[l_idx] > height[r_idx]: # move the shorter line so that area can may get larger
-            r_idx -= 1
+    while (l < r): # loop ends when r_idx - l_idx = 1
+        area_i = min(height[l], height[r]) * (r - l) # shorter height * length of base
+        area_max = max(area_max, area_i) # update max area
+        if height[l] > height[r]: # move the shorter line so that area can may get larger
+            r -= 1
         else:
-            l_idx += 1
+            l += 1
     return area_max
 
