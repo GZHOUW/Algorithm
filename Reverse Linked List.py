c99f8f43fb630946e34db6b2616463c750e4bc11
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-04-16 13:00:26 -0400

    Linked List, Recursion

--- Reverse Linked List.py
+++ Reverse Linked List.py
@@ -22,6 +22,26 @@ class Solution:
             temp.next = res # temp = 1 None
             res = temp # temp = 1 None
         return res
+
+    def reverseRecur(self, head: ListNode) -> ListNode:
+        if head == None:
+            return None
+        
+        self.head = head
+        self.helper(head)
+        return self.head
     
-    def reverseRecur(self, head):
-        return
+    def helper(self, curr): # curr = [1,2,3,4,5,None]
+        if curr.next == None:
+            self.head = curr # this line is run when the last node is reached. Set it as head of reversed list
+            return # return to the previous level
+        
+        self.helper(curr.next) # go to next level 
+        
+        curr.next.next = curr # this line is run when code returns to second to last level, i.e. cur = 4
+        # cur is 4, cur.next is 5, cur.next.next is None.
+        # Now set cur.next.next to 4, which means curr = 4 ----> 5 ------> 4
+        # Note that head is also pointed at 5
+        
+        curr.next = None # break the link between 4(curr) and 5 
+        # head = 5 ----> 4
