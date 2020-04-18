c11839cc31bd83023c368669c08419087b998275
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-04-18 10:25:27 -0400

    Dynamic Programming, Matrix

--- Minimum Path Sum.py
+++ Minimum Path Sum.py
@@ -15,17 +15,17 @@ Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 
 def minPathSum(grid):
     if len(grid) == 0:
-        return None
+            return None
     for r in range(len(grid)):
         for c in range(len(grid[0])):
-            if r == 0 and c == 0:
-                cost = grid[r][c]
-            elif r == 0: # r = 0 and c != 0
+            if r == 0 and c == 0: # upper left corner
+                cost = grid[r][c] # min cost for current cell
+            elif r == 0: # r = 0 and c != 0, top edge 
                 cost = grid[r][c] + grid[r][c-1]
-            elif c == 0: # r != 0 and c == 0
+            elif c == 0: # r != 0 and c == 0, left edge
                 cost = grid[r][c] + grid[r-1][c]
-            else: # r != 0 and c != 0
-                # min cost at a cell is equal to its own cost plus min(cost of cell above, cost of left)
+            else: # r != 0 and c != 0, everywhere else
+                # min cost at a cell = its own cost + min(cost of above cell , cost of left cell)
                 cost = grid[r][c] + min(grid[r-1][c], grid[r][c-1])
-            grid[r][c] = cost
+            grid[r][c] = cost # update grid matrix in-place into a cost matrix
     return grid[-1][-1]
