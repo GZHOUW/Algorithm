86d8ee625a3e60d5a807af970865a20abc4d8d81
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-04-28 11:42:57 -0400

    String

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -1,3 +1,15 @@
+'''
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+
+Example 1:
+Input: "III"
+Output: 3
+
+Example 2:
+Input: "IV"
+Output: 4
+'''
+
 def romanToInt(s):
     """
     :type s: str
