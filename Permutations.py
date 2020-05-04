dfe607d41498cee3c4e618fb09fb285991f7b121
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-05-04 12:01:31 -0400

    Backtracking

---
+++ Permutations.py
@@ -0,0 +1,51 @@
+'''
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+
+Demo:
+
+dfs(nums = [1, 2, 3] , path = [] , result = [] )
+|____ dfs(nums = [2, 3] , path = [1] , result = [] )
+|      |___dfs(nums = [3] , path = [1, 2] , result = [] )
+|      |    |___dfs(nums = [] , path = [1, 2, 3] , result = [[1, 2, 3]] ) # added a new permutation to the result
+|      |___dfs(nums = [2] , path = [1, 3] , result = [[1, 2, 3]] )
+|           |___dfs(nums = [] , path = [1, 3, 2] , result = [[1, 2, 3], [1, 3, 2]] ) # added a new permutation to the result
+|____ dfs(nums = [1, 3] , path = [2] , result = [[1, 2, 3], [1, 3, 2]] )
+|      |___dfs(nums = [3] , path = [2, 1] , result = [[1, 2, 3], [1, 3, 2]] )
+|      |    |___dfs(nums = [] , path = [2, 1, 3] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3]] ) # added a new permutation to the result
+|      |___dfs(nums = [1] , path = [2, 3] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3]] )
+|           |___dfs(nums = [] , path = [2, 3, 1] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1]] ) # added a new permutation to the result
+|____ dfs(nums = [1, 2] , path = [3] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1]] )
+       |___dfs(nums = [2] , path = [3, 1] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1]] )
+       |    |___dfs(nums = [] , path = [3, 1, 2] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2]] ) # added a new permutation to the result
+       |___dfs(nums = [1] , path = [3, 2] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2]] )
+            |___dfs(nums = [] , path = [3, 2, 1] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]] ) # added a new permutation to the result
+'''
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.findPerm(res, nums, [])
+        return res
+    
+    def findPerm(self, res, nums, subset):
+        if len(nums) == 0:  # all elements are added to subset
+            res.append(subset)
+        for i in range(len(nums)):
+            exclude_i = nums[:i] + nums[i+1:] # every element in nums except for nums[i]
+            new_subset = subset + [nums[i]]  # add the current number to subset
+            self.findPerm(res, exclude_i, new_subset)
+        
