adc1883ae2ae54f5cce9d841c341640b07aedc83
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-05-15 12:16:24 -0400

    Dynamic Programming, Array

--- Maximum Subarray.py
+++ Maximum Subarray.py
@@ -10,13 +10,17 @@ Explanation: [4,-1,2,1] has the largest sum = 6.
 
 # dynamic programming
 def maxSubArray(nums):
-    # if all elements are positive, maxSubArray is entire list
-    # if all elements are negative, maxSubArray is the max number
-    dp = [nums[0]]
+    ''' Demo
+    [0, 1, -3, 4, -1, 2, 1, -5, 4]
+    [0, 1, -2, 4,  3, 5, 6,  1, 5]
+       new    new       max
+
+    '''
+    dp = [nums[0]] # Each element is the sum of current subarray
     res = nums[0]
     for i in range(1, len(nums)):
-        if dp[i - 1] < 0: # dp[i - 1] is the sum of current subArray
-            dp.append(nums[i]) # start a new subArray, discard the previous
+        if dp[i - 1] <= 0: # If the previous sum is negative or 0, break that subarray, start a new one
+            dp.append(nums[i])
         else:
             dp.append(nums[i] + dp[i - 1]) # add nums[i] to current subArray
         res = max(res, dp[i]) # update result
