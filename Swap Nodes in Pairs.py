f925298e24acb21e9667b07cc5fba90551042812
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-05-16 12:28:11 -0400

    Linked List

--- Swap Nodes in Pairs.py
+++ Swap Nodes in Pairs.py
@@ -6,7 +6,10 @@ You may not modify the values in the list's nodes, only nodes itself may be chan
 Example:
 Given 1->2->3->4, you should return the list as 2->1->4->3.
 '''
-
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 def swapPairs(head):
     if head == None:
@@ -16,13 +19,17 @@ def swapPairs(head):
     n1 = s.next # first node of current pair
     n2 = s.next.next # second node of current pair
     prev = s # the previous node of the current pair 
+    
+    #    p   n1   n2  next 
+    #    0   1    2    3    4    5 
+    
     while n2 != None: 
-        nextPair = n2.next # the start of next pair
+        nextPair = n2.next # the start of next pair nextPair = 3 4 5
 
         # swap n1 and n2
-        prev.next = n2
-        n2.next = n1
-        n1.next = nextPair
+        prev.next = n2  # prev = 0 2 3 4 5
+        n2.next = n1  # n2 = 2 1 2 3 4 5
+        n1.next = nextPair  # n1 = 1 3 4 5 --> n2 = 2 1 3 4 5 (swapped)
 
         # n1 and n2 are now swapped
         prev = n1 # n1 is now after n2, set it as prev
