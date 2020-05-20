a0854b366adc18e3f8207a3789647e6889466654
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-05-20 11:28:49 -0400

    Backtracking

--- Letter Combinations of a Phone.py
+++ Letter Combinations of a Phone.py
@@ -10,7 +10,7 @@ Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 Note:
 
 Although the above answer is in lexicographical order, your answer could be in any order you want.
-'''
+
 
 def letterCombinations(digits):
     if digits == "":
@@ -33,30 +33,22 @@ def letterCombinations(digits):
             else: # 4 digits
                 res[j] += letters[digit_i][3]
     return res
+''' 
     
-''' better solution
-def letterCombinations(digits): 
-    phone = {'2':['a','b','c'], '3':['d','e','f'], '4':['g','h','i'], 
-               '5':['j','k','l'], '6':['m','n','o'], '7':['p','q','r','s'],
-               '8':['t','u','v'], '9':['w','x','y','z']}
-
-
-    def backtrack(combination, next_digits):
-        # if there is no more digits to check
-        if len(next_digits) == 0:
-            # the combination is done
-            output.append(combination)
-        # if there are still digits to check
-        else:
-            # iterate over all letters which map 
-            # the next available digit
-            for letter in phone[next_digits[0]]:
-                # append the current letter to the combination
-                # and proceed to the next digits
-                backtrack(combination + letter, next_digits[1:])
-
-    output = []
-    if digits:
-        backtrack("", digits)
-    return output
-'''
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]: 
+        phone = {'2':['a','b','c'], '3':['d','e','f'], '4':['g','h','i'], 
+                   '5':['j','k','l'], '6':['m','n','o'], '7':['p','q','r','s'],
+                   '8':['t','u','v'], '9':['w','x','y','z']}
+        res = []
+        if digits:
+            self.backtrack("", digits, res, phone)
+        return res
+    
+    def backtrack(self, subset, digits, res, phone):
+        if len(digits) == 0:  # if there is no more digits to check
+            res.append(subset)                         
+        else: # if there are still digits to check
+            for letter in phone[digits[0]]:  # iterate over [a,b,c]
+                self.backtrack(subset + letter, digits[1:], res, phone)# append the current letter to the combination
+                                                                # and proceed to the next digits
