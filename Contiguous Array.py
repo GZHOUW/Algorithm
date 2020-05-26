67cbc349cee5d55ef5d5ff5c2c6a163246e332b5
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-05-26 11:55:43 -0400

    Hash Table

--- Contiguous Array.py
+++ Contiguous Array.py
@@ -15,31 +15,38 @@ Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal numb
 def findMaxLength(nums):
     '''
     Demo: [1,0,0,1]
-    idx = -1 ------> count =  0
-    idx =  0 ------> count =  1
-    idx =  1 ------> count =  0
-    idx =  2 ------> count = -1
-    idx =  3 ------> count =  0
+    idx = -1 ----> count =  0 ---> {0:-1}
+    idx =  0 ----> count =  1 ---> {0:-1}
+    idx =  1 ----> count =  0 ---> {0:-1, 1:0} record length = 1-(-1) = 2
+    idx =  2 ----> count = -1 ---> {0:-1, 1:0, -1:2}
+    idx =  3 ----> count =  0 ---> {0:-1, 1:0, -1:2} record length = 3 - (-1) = 4
 
-    find the largest difference between two idx with same count:
-    3 - (-1) = 4
+    return max length = 4
     '''
-    
+
+
+    '''
+    itialize count to 0 and traverse through nums.If nums = 0, decrease count by 1, if num =1, increase count by 1. 
+    A contiguous subarray with equal number of 0 and 1 is found when count equals 0.
+    '''
     count = 0
-    countDict = dict()
+    countDict = dict()  # {count:index of that count}
+
     countDict[count] = -1  # the initial "index" need to be -1, because when idx=0, count becomes -1 or 1
-    length = []
+    length = []  # contains the length of all found contiguous arrays
     for i in range(len(nums)):
         if nums[i] == 0:
             count -= 1
         else:
             count += 1
 
-        if count not in countDict:
-            countDict[count] = i # new count value, add to dict
-        else: # back at same count, should record
-            length.append(i - countDict[count])# the largest difference between two idx with same count is the answer
+        if count not in countDict:  # new count value, add to dict
+            countDict[count] = i 
+        else:  # back at same count, contiguous array found, record
+            length.append(i - countDict[count])  # the largest difference between two idx with same count is the answer
+
     if length:
         return max(length)
     else: # all 1 or all 0
         return 0
+
