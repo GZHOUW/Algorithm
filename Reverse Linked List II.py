27fc6a0989fd094e76f73b7ebe31a367827df76f
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-06-11 14:41:03 -0400

    Linked List

--- Reverse Linked List II.py
+++ Reverse Linked List II.py
@@ -17,21 +17,31 @@ class Solution:
     def reverseBetween(self, head, m, n):
         if not head:
             return head
-        cur, prev = head, None
-        for _ in range(m-1):
-            prev = cur  # prev -->1
-            cur = cur.next # cur --> 2
-        tail = cur # tail --> 2
-        con = prev # con --> 1
-        for _ in range(n-m+1):
-            next = cur.next # next --> 3 --> 4 --> 5 --> 6
-            cur.next = prev # form circular list: 2 --> 1 --> 2 --> 1 ....
-            # move prev and cur one step forward
-            prev = cur # prev --> 2 --> 1 --> 2 --> 1 ...
-            cur = next # cur --> 3 --> 4 --> 5 --> 6
-        if con:
-            con.next = prev
-        else:
+        
+        cur = head # main pointer
+        prev = None # always 1 before cur
+        
+        # Move cur to m position and prev to one before
+        for _ in range(m-1): # runs once for m=2
+            prev = cur
+            cur = cur.next
+        
+        mNode = cur # the node at m, will be connected to n+1 node
+        mPrev = prev # the node before m node
+        
+        for _ in range(m, n+1):
+            nex = cur.next # points at one after cur
+            cur.next = prev # connect cur with previous element == reverse
+            
+            # move prev and cur one step forward for next iteration
+            prev = cur
+            cur = nex
+        
+        # At this point, 1:n are correctly reversed, but not connected with n+1:end
+        # e.g. prev = 4->3->2->1    cur = 5->6->None
+        if mPrev:
+            mPrev.next = prev
+        else: # nPrev is None --> reverse only the first node
             head = prev
-        tail.next =  next
+        mNode.next =  cur
         return head
