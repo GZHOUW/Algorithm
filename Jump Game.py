ea4b8d060015d738148663fde20299ed97690925
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-06-11 21:46:02 -0400

    Array, Greedy

--- Jump Game.py
+++ Jump Game.py
@@ -1,31 +1,31 @@
 '''
 Given an array of non-negative integers, you are initially positioned at the first index of the array.
-
 Each element in the array represents your maximum jump length at that position.
-
 Determine if you are able to reach the last index.
 
 Example 1:
-Input: [2,3,1,1,4]
-Output: true
-Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+  Input: [2,3,1,1,4]
+  Output: true
+  Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 
 Example 2:
-Input: [3,2,1,0,4]
-Output: false
-Explanation: You will always arrive at index 3 no matter what. Its maximum
-             jump length is 0, which makes it impossible to reach the last index.
+  Input: [3,2,1,0,4]
+  Output: false
+  Explanation: You will always arrive at index 3 no matter what. Its maximum
+               jump length is 0, which makes it impossible to reach the last index.
 '''
 
 def canJump(nums):
-    end = len(nums) - 1
-    max_reach = 0
+    end = len(nums) - 1 # the last index
+    max_reach = 0  # the furthest position that [0:cur] can reach. 
+                   # Ex: [3,0,X,X,X] if cur=1(0), maxReach=3 because of 3 at idx 0
     for i, step in enumerate(nums):
         if max_reach >= end: # max reach is/exceeds the end
             return True
         elif max_reach < i: # max_reach stayed at i-1, cannot reach current index i
             return False
         max_reach = max(max_reach, i + step)
+
 
 '''
 Demo:
