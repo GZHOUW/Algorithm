02b9332af8a27d0796739647563e885a64606fd9
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-06-19 12:06:42 -0400

    Tree, Recursion

--- Validate Binary Search Tree.py
+++ Validate Binary Search Tree.py
@@ -1,6 +1,5 @@
 '''
 Given a binary tree, determine if it is a valid binary search tree (BST).
-
 Assume a BST is defined as follows:
 
 The left subtree of a node contains only nodes with keys less than the node's key.
@@ -9,13 +8,13 @@ Both the left and right subtrees must also be binary search trees.
  
 
 Example 1:
-
     2
    / \
   1   3
 
-Input: [2,1,3]
-Output: true
+ Input: [2,1,3]
+ Output: true
+
 Example 2:
 
     5
@@ -24,33 +23,34 @@ Example 2:
      / \
     3   6
 
-Input: [5,1,4,null,null,3,6]
-Output: false
-Explanation: The root node's value is 5 but its right child's value is 4.
+ Input: [5,1,4,null,null,3,6]
+ Output: false
+ Explanation: The root node's value is 5 but its right child's value is 4.
 '''
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+      
 class Solution:
-        
     def isValidBST(self, root):
-        self.nodeValues = []
-        self.inOrderTraverse(root)
-
-        return self.isSorted(self.nodeValues)
+        self.nodeValues = [] # store the value of nodes in the order of visiting them
+        self.inOrderTraverse(root) # perform in order traversal
+        return self.isSorted(self.nodeValues) # if values are sorted, BST is valid
     
     def inOrderTraverse(self, root):
         if root:
-            self.inOrderTraverse(root.left)
-            self.nodeValues.append(root.val)
-            self.inOrderTraverse(root.right)
+            self.inOrderTraverse(root.left) # go all the way left
+            self.nodeValues.append(root.val) # visit node
+            self.inOrderTraverse(root.right) # go right
+        else: # cur node doesnt have a left/right child, return to previous level
+            return 
     
     def isSorted(self, L):
         for i in range(len(L)-1):
             if L[i] >= L[i+1]:
                 return False
-        return True;
+        return True
