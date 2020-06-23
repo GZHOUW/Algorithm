67b8ceae7833f27b1d153755e27147f9dc39f406
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-06-23 10:34:23 -0400

    Bit Manipulation

--- LeetCode Problems/Single Number.py
+++ LeetCode Problems/Single Number.py
@@ -1,26 +1,26 @@
 '''
 Given a non-empty array of integers, every element appears twice except for one. Find that single one.
-
-Note:
-
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
 Example 1:
-
-Input: [2,2,1]
-Output: 1
+    Input: [2,2,1]
+    Output: 1
+    
 Example 2:
-
-Input: [4,1,2,1,2]
-Output: 4
+    Input: [4,1,2,1,2]
+    Output: 4
 '''
 
 def singleNumber(nums):
-    single = 0
-    for i in range(len(nums)):
-        single ^= nums[i]
-    return single
-
-# 1. a⊕0=a
-# 2. a⊕a=0
-# therefore, a⊕b⊕a=(a⊕a)⊕b=0⊕b=b
+    '''
+    ^ Bitwise Exclusive XOR:
+    Convert two integers to binary form,
+    then compare them bit by bit.
+    If two bits are the same, XOR outputs 0. 
+    If the bits are different, XOR outputs 1.
+    '''
+    totalXOR = 0
+    for num in nums:
+        # 0^1^2^3^1^2^3^4 = 0^1^1^2^2^3^3^4 = 0^4 = 4
+        totalXOR = totalXOR ^ num
+    return totalXOR
