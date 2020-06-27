0294c357e87e144743e771248c4781fa861ffed1
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-06-27 13:25:07 -0400

    Dynamic Programming

--- LeetCode Problems/Perfect Squares.py
+++ LeetCode Problems/Perfect Squares.py
@@ -12,15 +12,18 @@ Output: 2
 Explanation: 13 = 4 + 9.
 '''
 
-def numSquares(n):
-    nSquare = [x for x in range(n+1)]
-
-    for i in range(2, len(nSquare)):  # 0 and 1 are base cases
-        for j in range(1, i):
-            remain = i - j*j  # one perfect square number used, count into result
-            if remain < 0:
-                break
-            else:
-                nSquare[i] = min(nSquare[i], nSquare[remain] + 1) # +1 account for the one used in calculating remain
-
-    return nSquare[n]  # last element corresponds to n
+class Solution:
+    def numSquares(self, n):
+        nSquare = [x for x in range(n+1)] # nSquare[i] stores the least number of PSNs which sum to i
+    
+        for i in range(2, len(nSquare)):  # 0 and 1 are base cases
+            # try subtracking PSNs from cur number, find the PSN that is as large as possible without making remain negative
+            for j in range(1, i):
+                PSN = j * j 
+                remain = i - PSN  # one perfect square number used, count into result
+                if remain < 0:
+                    break
+                else:
+                    nSquare[i] = min(nSquare[i], nSquare[remain] + 1) 
+                    
+        return nSquare[n]  # last element corresponds to n
