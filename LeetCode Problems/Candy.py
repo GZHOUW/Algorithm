8b5eec8f2628ee7f31ae5c3714f40a19071ff92b
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-06-27 12:03:38 -0400

    Greedy

--- LeetCode Problems/Candy.py
+++ LeetCode Problems/Candy.py
@@ -20,14 +20,21 @@ Example 2:
 class Solution:
     def candy(self, ratings):
         candy = [1 for _ in ratings]
+        
+        # if a child has high rating than his LEFT neighbor, give him candies until he has 1 more than his LEFT neighbor
+        # e.g. rating = [1,0,2], candy = [1,1,1] --> [1,1,2]
         for i in range(1, len(candy)):
             if ratings[i] > ratings[i-1]:
                 candy[i] = candy[i-1] + 1
         
+        # if a child has high rating than his RIGHT neighbor, either do nothing or give him candies until he has 1 more than his RIGHT neighbor
+        # depending on which number is higher
+        # candy = [1,1,2] --> [2,1,2]
         for j in range(len(candy)-2, -1, -1):
             if ratings[j] > ratings[j+1]:
                 candy[j] = max(candy[j], candy[j+1] + 1)
-                
+                                                                  
         return sum(candy)
         
         
+            
