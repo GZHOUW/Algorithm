f25110f8781f7b9288eade85401a43a6e8bd7237
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-07-01 12:50:56 -0400

    Dynamic Programming

---
+++ LeetCode Problems/Decode Ways.py
@@ -0,0 +1,39 @@
+'''
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given a non-empty string containing only digits, determine the total number of ways to decode it.
+
+Example 1:
+    Input: "12"
+    Output: 2
+    Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+
+Example 2:
+  Input: "226"
+  Output: 3
+  Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+'''
+
+class Solution:
+    def numDecodings(self, s):
+        if not s or s[0] == "0":
+            return 0
+
+        dp = [0 for _ in range(len(s) + 1)] 
+
+        # base case initialization
+        dp[0] = 1 
+        dp[1] = 1 # only one way to decode a single-digit number
+
+        for i in range(2, len(s) + 1): # must start with 2 becasue of s[i-2:i]
+            if 0 < int(s[i-1]) <= 9: # One-digit number s[i-1]
+                dp[i] += dp[i - 1]
+            if 10 <= int(s[i-2:i]) <= 26: # Two-digit number 
+                dp[i] += dp[i - 2]
+        
+        return dp[-1]
+
