1dab7fedc8874d6e6af52947f94d9bdb7da76b86
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-07-06 11:45:00 -0400

    Two Pointers, Sort

--- LeetCode Problems/Sort Colors.py
+++ LeetCode Problems/Sort Colors.py
@@ -11,28 +11,29 @@ Output: [0,0,1,1,2,2]
 
 '''
 
-def sortColors(nums) :
-    """
-    Demo:
-    [2,0,2,1,1,0] front=0, pointer=0, end=5
-    [0,0,2,1,1,2] front=0, pointer=0, end=4
-    [0,0,2,1,1,2] front=1, pointer=1, end=4
-    [0,0,2,1,1,2] front=2, pointer=2, end=4
-    [0,0,1,1,2,2] front=2, pointer=2, end=3
-    [0,0,1,1,2,2] front=3, pointer=3, end=3
-    """
-
-    front = 0 # points at the position where the next 0 is supposed to be
-    pointer = 0 # travels from 0 to end
-    end = len(nums) - 1 # points at the position where the next 2 is supposed to be
-
-    while pointer <= end:
-        if nums[pointer] == 0: # move to front
-            nums[front], nums[pointer] = nums[pointer], nums[front]
-            front += 1
-            pointer += 1
-        elif nums[pointer] == 2: # move to end
-            nums[end], nums[pointer] = nums[pointer], nums[end]
-            end -= 1
-        else: # find 1, dont move, wait for 0 to swap with 1
-            pointer += 1
+class Solution:
+    def sortColors(self, nums):
+        """
+        Demo:
+        [2,0,2,1,1,0] front=0, pointer=0, end=5
+        [0,0,2,1,1,2] front=0, pointer=0, end=4
+        [0,0,2,1,1,2] front=1, pointer=1, end=4
+        [0,0,2,1,1,2] front=2, pointer=2, end=4
+        [0,0,1,1,2,2] front=2, pointer=2, end=3
+        [0,0,1,1,2,2] front=3, pointer=3, end=3
+        """
+        
+        front = 0 # the position where the next 0 is supposed to be
+        pointer = 0 # travels from 0 to end
+        end = len(nums) - 1 # the position where the next 2 is supposed to be
+        
+        while pointer <= end:
+            if nums[pointer] == 0: # finds 0, move to front
+                nums[front], nums[pointer] = nums[pointer], nums[front]
+                front += 1
+                pointer += 1
+            elif nums[pointer] == 2: # finds 2, move to end
+                nums[end], nums[pointer] = nums[pointer], nums[end]
+                end -= 1
+            else: # finds 1, do nothing
+                pointer += 1
