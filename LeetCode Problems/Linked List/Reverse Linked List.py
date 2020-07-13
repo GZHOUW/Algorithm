464e5ba34d883ee1cfc26e7a15d6fbd445e50717
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-07-13 15:20:13 -0400

    Update Reverse Linked List.py

--- LeetCode Problems/Linked List/Reverse Linked List.py
+++ LeetCode Problems/Linked List/Reverse Linked List.py
@@ -14,35 +14,15 @@ class ListNode:
         self.next = None
 
 class Solution:
-    def reverseIter(self, head):
-        res = None
-        while head != None:
-            # remove the first element from head and append it to res
-            temp = head # 1 2 3 4 5 None
-            head = head.next # 2 3 4 5 None
-            temp.next = res # temp = 1 None
-            res = temp # temp = 1 None
-        return res
-
-    def reverseRecur(self, head: ListNode) -> ListNode:
-        if head == None:
-            return None
+    def reverseList(self, head):
+        if not head or not head.next: # when the end of list is reached by recursion
+            return head 
         
-        self.head = head
-        self.helper(head)
-        return self.head
+        next = head.next # head=4, next=5
+        newHead = self.reverseList(next) # traverse all the way to end of list, newHead=5
+        next.next = head # 5.next = 4,  5--->4
+        head.next = None # 5-->4-->None
+        
+        return newHead
     
-    def helper(self, curr): # curr = [1,2,3,4,5,None]
-        if curr.next == None:
-            self.head = curr # this line is run when the last node is reached. Set it as head of reversed list
-            return # return to the previous level
-        
-        self.helper(curr.next) # go to next level 
-        
-        curr.next.next = curr # this line is run when code returns to second to last level, i.e. cur = 4
-        # cur is 4, cur.next is 5, cur.next.next is None.
-        # Now set cur.next.next to 4, which means curr = 4 ----> 5 ------> 4
-        # Note that head is also pointed at 5
-        
-        curr.next = None # break the link between 4(curr) and 5 
-        # head = 5 ----> 4
+   
