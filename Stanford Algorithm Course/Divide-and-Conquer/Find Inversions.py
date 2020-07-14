978bc5ace9c42b27eb825f071f2614a294776a88
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-07-14 14:11:55 -0400

    Fixed Bugs

--- Stanford Algorithm Course/Divide-and-Conquer/Find Inversions.py
+++ Stanford Algorithm Course/Divide-and-Conquer/Find Inversions.py
@@ -1,54 +1,52 @@
-
-def sortCountInv(A):
+def countInv(A):
     '''
     :param A: list of distinct integers
-    :return: the sorted version of A,   the number of inversions in A
+    :return: the number of inversions in A
     '''
-
-    if len(A) == 0 or len(A) == 1: # no inversions
-        return A, 0
+    mid = len(A)//2
+    left = A[:mid]
+    right = A[mid:]
+    if len(A) <= 1: # no inversions
+        return 0
     else:
         # all left and right invs will eventually become split invs
-        lSorted, lInv = sortCountInv(A[:len(A)//2])
-        rSorted, rInv = sortCountInv(A[len(A)//2:])
-        allSorted, splitInv = mergeCountSplitInv(lSorted, rSorted)
-
-        invCount = lInv + rInv + splitInv
-        return allSorted, invCount
+        lInv = countInv(left)
+        rInv = countInv(right)
+        splitInv = countSplitInv(left, right, A)
+        return lInv + rInv + splitInv
 
 
-def mergeCountSplitInv(lSorted, rSorted):
+def countSplitInv(left,right,array):
     '''
     :param lSorted: sorted list with length n
     :param rSorted: sorted list with length n
     :return: the merge sorted version of lSorted and rSorted, the number of split inversions in lSorted and rSorted
     '''
-    splitInv = 0
-    allSorted = [None] * (len(lSorted)+len(rSorted))
-    l = 0
-    r = 0
-    idx = 0
-    while l < len(lSorted) and r < len(rSorted):
-        if lSorted[l] < rSorted[r]:
-            allSorted[idx] = lSorted[l]
-            l += 1
+    i = j = k = count = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            array[k] = left[i]
+            i = i + 1
         else:
-            allSorted[idx] = rSorted[r]
-            if r <= l: # if the second half's element is added to allSorted before first half's, a split inv is found
-                splitInv += 1
-            r += 1
-        idx += 1
+            array[k] = right[j]
+            j = j + 1
+            count = count + len(left) - i
+        k = k + 1
 
-    while l < len(lSorted):
-        allSorted[idx] = lSorted[l]
-        l += 1
-        idx += 1
+    while i < len(left):
+        array[k] = left[i]
+        i = i + 1
+        k = k + 1
+    while j < len(right):
+        array[k] = right[j]
+        j = j + 1
+        k = k + 1
 
-    while r < len(rSorted):
-        allSorted[idx] = rSorted[r]
-        r += 1
-        idx += 1
-    return allSorted, splitInv
+    return count
 
-
-print(sortCountInv([1,2,6,3,4,5,7]))
+text_file = open("nums.txt", "r")
+lines = text_file.read().split('\n')
+for i in range(len(lines)):
+    lines[i]= int(lines[i])
+a = countInv(lines)
+print(a)
