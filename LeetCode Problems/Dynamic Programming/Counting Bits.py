c0e5654d7e1dd8a2d2a2deef70d5f3e5ff6c497c
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-07-17 16:06:27 -0300

    Update Counting Bits.py

--- LeetCode Problems/Dynamic Programming/Counting Bits.py
+++ LeetCode Problems/Dynamic Programming/Counting Bits.py
@@ -11,18 +11,28 @@ Example 2:
   Output: [0,1,1,2,1,2]
 '''
 
-def countBits(num):
-    '''
-    Demo: 
-    0
-    0 1
-    0 1 1 2
-    0 1 1 2 1 2 2 3
-    '''
-    res = [0]
-    while len(res)<=num:
-        len_i = len(res)
-        # copy all existing element in res, add one to each, and append to the end
-        for i in range(len_i):  # cannot use for nBit in res because len keep changing
-            res.append(res[i] + 1)
-    return res[:num+1] # one more than num because of 0
+class Solution:
+    def countBits(self, num):
+        '''
+        Index : 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+        num :   0 1 1 2 1 2 2 3 1 2  2  3  2  3  3  4
+        
+        dp[0] = 0;          dp[0] = 0;
+        dp[1] = dp[0] + 1;  dp[1] = dp[1-1] + 1;
+        dp[2] = dp[0] + 1;  dp[2] = dp[2-2] + 1;
+        dp[3] = dp[1] + 1;  dp[3] = dp[3-2] +1;
+        dp[4] = dp[0] + 1;  dp[4] = dp[4-4] + 1;
+        dp[5] = dp[1] + 1;  dp[5] = dp[5-4] + 1;
+        dp[6] = dp[2] + 1;  dp[6] = dp[6-4] + 1;
+        dp[7] = dp[3] + 1;  dp[7] = dp[7-4] + 1;
+        dp[8] = dp[0] + 1;  dp[8] = dp[8-8] + 1;
+        '''
+        res = [0 for _ in range(num+1)]
+        offset = 1
+        
+        for i in range(1, num+1):
+            if offset * 2 == i:
+                offset *= 2
+        
+            res[i] = res[i - offset] + 1
+        return res
