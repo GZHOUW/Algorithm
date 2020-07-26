4a56c8f8a0d14a94bb96d2c704494a4f3f899103
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-07-26 11:54:14 -0300

    Create Find Minimum in Rotated Sorted Array II.py

---
+++ LeetCode Problems/Binary Search/Find Minimum in Rotated Sorted Array II.py
@@ -0,0 +1,33 @@
+'''
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+Find the minimum element.
+The array may contain duplicates.
+
+Example 1:
+    Input: [1,3,5]
+    Output: 1
+
+Example 2:
+    Input: [2,2,2,0,1]
+    Output: 0
+'''
+
+class Solution:
+    def findMin(self, nums):
+        left= 0
+        right = len(nums) - 1
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            
+            if nums[mid] > nums[right]: # mid is in left segment, right is in right segment
+                left = mid + 1
+            
+            elif nums[mid] < nums[right]: # mid and right are in same segment, can be either left or right
+                right = mid
+            
+            else: # when nums[mid] and nums[right] are same (duplicate)
+                right-= 1
+
+        return nums[left]
