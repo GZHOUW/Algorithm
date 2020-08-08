3dc980ab6538220c6591c852bc6c0644bf734032
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-08-08 11:06:23 -0400

    Update Group Anagrams.py

--- LeetCode Problems/Group Anagrams.py
+++ LeetCode Problems/Group Anagrams.py
@@ -2,14 +2,14 @@
 Given an array of strings, group anagrams together.
 
 Example:
-
-Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
-Output:
-[
-  ["ate","eat","tea"],
-  ["nat","tan"],
-  ["bat"]
-]
+    Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+    Output:
+    [
+      ["ate","eat","tea"],
+      ["nat","tan"],
+      ["bat"]
+    ]
+    
 Note:
 
 All inputs will be in lowercase.
@@ -17,9 +17,17 @@ The order of your output does not matter.
 '''
 
 class Solution:
-    # count freq method: time complexity O(n)
-    def groupAnagrams(self, strs):
-        anagrams = dict() # key = a base anagram, value: a list containing all the anagram of the base case
+    '''
+    Algorithm:  1. Create a dict where key is a base string and value is a list containing all the anagrams of the base string
+                2. For every string in strings, do the following:
+                    2.1. Create a len=26 frequency list that contains how many times each char appear. E.g. "addf"-->[1,0,0,2,0,1,...]
+                    2.2. Convert frequency list to a frequency string. E.g. [1,0,0,2,0,1,...]--> "1a2d1f"
+                    2.3. Let frequency string be the key of dict and the string itself as the val
+                3. Transform the dict into list and return
+    
+    '''
+    def groupAnagrams(self, strs): # count freq method: time complexity O(n)
+        anagrams = dict() # key = a base string, value: a list containing all the anagram of the base string
         for string in strs:
             # create a dict of freq of string's chars
             freq = [0] * 26
@@ -28,8 +36,9 @@ class Solution:
             freqStr = ''
             for i in range(len(freq)):
                 if freq[i] != 0:
-                    freqStr += chr(i + ord('a'))
                     freqStr += str(freq[i])
+                    freqStr += chr(i + ord('a'))
+                    
             if freqStr not in anagrams:
                 anagrams[freqStr] = [string]
             else:
