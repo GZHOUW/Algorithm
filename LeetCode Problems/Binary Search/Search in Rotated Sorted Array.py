72af15715b12446d29f1704a58be30baa64ff0a2
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-08-09 11:06:54 -0400

    Update Search in Rotated Sorted Array.py

--- LeetCode Problems/Binary Search/Search in Rotated Sorted Array.py
+++ LeetCode Problems/Binary Search/Search in Rotated Sorted Array.py
@@ -14,7 +14,43 @@ Example 2:
 Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
 '''
-
+class Solution:
+    def search(self, nums, target):
+        if not nums:
+            return -1
+        l = 0
+        r = len(nums)-1
+        
+        # Perform binary search to find the partition point
+        while l <= r:
+            m = (r-l)//2 + l
+            if nums[m] <= nums[-1]: #go left
+                r = m - 1
+            else: # [2,3,1] go right
+                l = m + 1
+                
+        # At this point, left is the first index of the right part (min value of nums)
+        partition = l
+        
+        # Determine target in whivh part and perform another binary search for target
+        if nums[partition] <= target <= nums[-1]: # target in right part
+            left = partition
+            right = len(nums) - 1
+        else: # target in left part
+            left = 0
+            right = partition - 1
+            
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return -1
+'''
 def search(nums, target):
     length = len(nums)
     if length == 0:
@@ -45,3 +81,4 @@ def search(nums, target):
         return r
     else:
         return -1
+'''
