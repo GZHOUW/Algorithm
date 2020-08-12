5d59f0548e1679b0519e3c60d41bb2aeac668902
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-08-12 13:33:00 -0400

    Update Minimum Path Sum.py

--- LeetCode Problems/Dynamic Programming/Minimum Path Sum.py
+++ LeetCode Problems/Dynamic Programming/Minimum Path Sum.py
@@ -12,20 +12,18 @@ Input:
 Output: 7
 Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 '''
+class Solution():
+    '''
+    Algorithm: create in-place dp where dp[r][c] is the min path to arrive at grid[i][c]
+    '''
+    def minPathSum(self, grid):
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if r == 0 and c!= 0:
+                    grid[r][c] = grid[r][c] + grid[r][c-1]
+                elif r != 0 and c == 0:
+                    grid[r][c] = grid[r][c] + grid[r-1][c]
+                elif r!= 0 and c!=0:
+                    grid[r][c] = min(grid[r-1][c], grid[r][c-1]) + grid[r][c]
+        return grid[-1][-1]
 
-def minPathSum(grid):
-    if len(grid) == 0:
-            return None
-    for r in range(len(grid)):
-        for c in range(len(grid[0])):
-            if r == 0 and c == 0: # upper left corner
-                cost = grid[r][c] # min cost for current cell
-            elif r == 0: # r = 0 and c != 0, top edge 
-                cost = grid[r][c] + grid[r][c-1]
-            elif c == 0: # r != 0 and c == 0, left edge
-                cost = grid[r][c] + grid[r-1][c]
-            else: # r != 0 and c != 0, everywhere else
-                # min cost at a cell = its own cost + min(cost of above cell , cost of left cell)
-                cost = grid[r][c] + min(grid[r-1][c], grid[r][c-1])
-            grid[r][c] = cost # update grid matrix in-place into a cost matrix
-    return grid[-1][-1]
