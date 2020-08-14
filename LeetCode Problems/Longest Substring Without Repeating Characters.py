3f0c1c1cb596b3ad18a687bf6730a9eff97104a3
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-08-13 22:12:04 -0400

    Update Longest Substring Without Repeating Characters.py

--- LeetCode Problems/Longest Substring Without Repeating Characters.py
+++ LeetCode Problems/Longest Substring Without Repeating Characters.py
@@ -19,20 +19,30 @@ Explanation: The answer is "wke", with the length of 3.
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 '''
 
-def lengthOfLongestSubstring(self, s):
-    """
-    :type s: str
-    :rtype: int
-    """
-    length = []
-    for i in range(len(s)):
-        substring = ''
-        idx = 0
-        while (i + idx < len(s)) and (s[i + idx] not in substring): 
-            # second condition is not evaluated if the first is False
-            substring += s[i + idx]
-            idx += 1
-        length.append(len(substring))
-    if length == []:
-        return 0
-    return max(length)
+class Solution:
+    '''
+    Algorithm: Keep a sliding window *substring) when iterating through the array, and keep a dictionary that contains the last occurence of every element
+               if the cur char has appeared before, set he left boundary as the char after the last occurence of cur char (start a new substring there to
+               avoid duplicate)
+               update length along the way
+    '''
+    def lengthOfLongestSubstring(self, s):
+        start = 0
+        cur = 0
+        idx = dict() # key = char, val = id of the char's last occurence
+        maxLen = 0
+        curLen = 0
+        
+        while cur < len(s):
+            #    duplicate   &  the previous occurence is after start (within the current substring)
+            if s[cur] in idx and idx[s[cur]]>= start:
+                maxLen = max(maxLen, curLen)
+                start  = idx[s[cur]]+1
+                
+            idx[s[cur]] = cur # {'a':1}
+            
+            curLen = cur-start + 1
+            print(curLen)
+            cur += 1
+        maxLen = max(maxLen, curLen)
+        return maxLen
